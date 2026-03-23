54465e03acb7cdf07c9a09c434846297da97e727
Alokswain <alokdhana123@gmail.com>
2026-03-23 13:17:04 +0530

    advance locator modified

--- tests/InterviewQuestionPratiace/AdvanceLocator.spec.js
+++ tests/InterviewQuestionPratiace/AdvanceLocator.spec.js
@@ -13,6 +13,7 @@
 //Click Edit button for Alok’s card only.
 //has method
 import {page} from '@playwright/test';
+import { TIMEOUT } from 'node:dns';
 
   page.locator('.card:has(h3:has-text("Alok"))')
   .locator('button').click();
@@ -44,3 +45,33 @@ page.locator('.row').filter({hasText:'Active'}).click();
 
 //Find the row where name = Alok, then click Edit.
 page.locator('.row').filter({has:page.locator('.name',{hasText:'Alok'})}).locator('button').click();
+
+/*
+<div class="card">
+  <h3 class="title">Alok Swain</h3>
+  <p class="role">Tester</p>
+  <button>View</button>
+</div>
+
+<div class="card">
+  <h3 class="title">Manish Joshi</h3>
+  <p class="role">Manager</p>
+  <button>View</button>
+</div>
+
+<div class="card">
+  <h3 class="title">Ashutosh</h3>
+  <p class="role">Director</p>
+  <button>View</button>
+</div>
+*/
+//All buttons have view text here the filter concept will come
+page.getByRole('button',{name:"View"}).click(); //It will always click the first button
+//How do we pick correct card :This is where filter({hasText}) were used.
+//✅ Scenario: Click View button inside card where name = “Manish Joshi”
+const card=page.locator('.card').filter({hasText:'Manish Joshi'});
+await card.getByRole('button',{name:'View'}).click();
+
+//Click View button for the card where role = “Director”. use advance filter({has})
+const card1=page.locator('.card').filter(has:page.locator('.role'{hasText:"Director"}));
+card1.getByRole('button',{name:'View'}).click({TIMEOUT:'500'});
